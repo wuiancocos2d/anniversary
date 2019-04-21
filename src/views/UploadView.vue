8970aef0ad185b767209e88352ccfb348c6df18a
wuiancocos2d <wuiancocos2d@gmail.com>
2019-04-21 08:08:36 +0800

    upload

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -1,63 +1,144 @@
 <template>
-<div>
-  <!-- bidirectional data binding（双向数据绑定） -->
-  <quill-editor v-model="content"
-                ref="myQuillEditor"
-                :options="editorOption"
-                @blur="onEditorBlur($event)"
-                @focus="onEditorFocus($event)"
-                @ready="onEditorReady($event)">
-  </quill-editor>
+  <div class="editor-block">
+    <div class="coverImg">
+      <a-upload
+        name="avatar"
+        listType="picture-card"
+        class="avatar-uploader"
+        :showUploadList="false"
+        action="//jsonplaceholder.typicode.com/posts/"
+        :beforeUpload="beforeUpload"
+        @change="handleChange"
+      >
+        <img v-if="imageUrl" :src="imageUrl" alt="avatar">
+        <div v-else>
+          <a-icon :type="loading ? 'loading' : 'plus'"/>
+          <div class="ant-upload-text">Upload Cover Image</div>
+        </div>
+      </a-upload>
+        <a-input placeholder="Please Input Title"/>
+      <div ref="editor" style="text-align:left"></div>
 
-  <!-- Or manually control the data synchronization（或手动控制数据流） -->
-  <quill-editor :content="content"
-                :options="editorOption"
-                @change="onEditorChange($event)">
-  </quill-editor>
+    </div>
+    <button v-on:click="getContent">查看内容</button>
   </div>
 </template>
 
 <script>
-
-  // you can also register quill modules in the component
-  import Quill from 'quill'
-  
-  export default {
-    data () {
-      return {
-        content: '<h2>I am Example</h2>',
-        editorOption: {
-          // some quill options
-        }
+import E from "wangeditor"
+import {Upload, Input} from "ant-design-vue"
+function getBase64 (img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+export default {
+  name: "editor",
+  data() {
+    return {
+      editorContent: ""
+    };
+  },
+  components: {
+    "a-upload": Upload,
+    "a-input": Input
+  },
+  methods: {
+    getContent: function() {
+      alert(this.editorContent);
+    },
+    handleChange (info) {
+      if (info.file.status === 'uploading') {
+        this.loading = true
+        return
+      }
+      if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, (imageUrl) => {
+          this.imageUrl = imageUrl
+          this.loading = false
+        })
       }
     },
-    // manually control the data synchronization
-    // 如果需要手动控制数据同步，父组件需要显式地处理changed事件
-    methods: {
-      onEditorBlur(quill) {
-        console.log('editor blur!', quill)
-      },
-      onEditorFocus(quill) {
-        console.log('editor focus!', quill)
-      },
-      onEditorReady(quill) {
-        console.log('editor ready!', quill)
-      },
-      onEditorChange({ quill, html, text }) {
-        console.log('editor change!', quill, html, text)
-        this.content = html
+    beforeUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        this.$message.error('You can only upload JPG file!')
       }
-    },
-    computed: {
-        'quill-editor': Quill,
-      editor() {
-        return this.$refs.myQuillEditor.quill
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isLt2M) {
+        this.$message.error('Image must smaller than 2MB!')
       }
+      return isJPG && isLt2M
     },
-    mounted() {
-      console.log('this is current quill instance object', this.editor)
+  },
+  mounted() {
+    var editor = new E(this.$refs.editor);
+    editor.customConfig.onchange = html => {
+      this.editorContent = html;
+    };
+    editor.customConfig.menus = [
+      "head", // 标题
+      "bold", // 粗体
+      "fontSize", // 字号
+      "fontName", // 字体
+      "italic", // 斜体
+      "underline", // 下划线
+      "strikeThrough", // 删除线
+      "foreColor", // 文字颜色
+      "backColor", // 背景颜色
+      "link", // 插入链接
+      "list", // 列表
+      "justify", // 对齐方式
+      "quote", // 引用
+      "emoticon", // 表情
+      "image", // 插入图片
+      "table", // 表格
+      // "video", // 插入视频
+      // "code", // 插入代码
+      "undo", // 撤销
+      "redo" // 重复
+    ];
+    editor.customConfig.lang = {
+      设置标题: "title",
+      字号码: "font-size",
+      字体: "font-family",
+      正文: "p",
+      链接文字: "link text",
+      链接: "link",
+      文字颜色: "font-color",
+      背景颜色: "background-color",
+      上传图片: "upload image",
+      网络图片: "online-link",
+      上传: "upload",
+      创建: "init",
+      插入: "insert"
+    };
+    editor.customConfig.uploadImgShowBase64 = true; // 使用 base64 保存图片
+    editor.create();
+  }
+}
+</script>
+<style lang="scss" scoped>
+.editor-block {
+  margin: 50px auto;
+  padding: 0 15px;
+  max-width: 1000px;
+  box-sizing: border-box;
+  .coverImg {
+    // background: #f7f8f9;
+    line-height: 192px;
+    color: #808080;
+    min-height: 192px;
+    text-align: center;
+  }
+  .avatar-uploader {
+    width: 100%;
+    height: 100%;
+    img {
+      width: 100%;
     }
   }
-</script>
-
+}
+</style>
 
